6a5a37d066e07ad17f3b87d0c5531b127ad8040a
azrajesh0007 <rajesh.azmeera@tesseractesports.com>
2024-11-12 13:37:21 +0530

    integration working

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import Format from "./components/TournamentTemplate/Components/Format.svelte";
+  import Section from "./components/TournamentTemplate/Components/Section.svelte";
   import BannerSection from "./components/TournamentTemplate/Components/Sliders/BannerSection.svelte";
 
 </script>
-<Format />
-<BannerSection />
+  <BannerSection />
--- src/Components/TournamentTemplate/Components/Sliders/BannerSection.svelte
+++ src/Components/TournamentTemplate/Components/Sliders/BannerSection.svelte
@@ -1,47 +1,29 @@
 <script>
-    import CardSlider from './CardSlider.svelte';
+    import Section from '../Section.svelte';
+import CardSlider from './CardSlider.svelte';
     import { Splide, SplideSlide } from '@splidejs/svelte-splide';
-  
-    const splideOptions = {
-      arrowPath: "M24 8L21.18 10.82L32.34 22H8V26H32.34L21.18 37.18L24 40L40 24L24 8Z",
-      gap: '2rem',
-      pagination: true,
-      autoplay: 'pause',
-      arrows: false,
-      perPage: 1,
-      
-    };
-  
+    export let data = {};
     // Array of banner data for looping
-    const banners = [
-      {
-        title: "Winners Concluded",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-        image: "/images/tournament-web-images/Group-1116601973.svg",
-        button1: { text: "Watch Live", icon: "/images/tournament-web-images/video-camera.svg" },
-        button2: { text: "Tournament details" },
-        icon: "/images/tournament-web-images/Union.svg",
-      },
-      {
-        title: "BGIS 2024",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
-        image: "/images/tournament-web-images/Group-1116601973.svg",
-        button1: { text: "Watch Live", icon: "/images/tournament-web-images/video-camera.svg" },
-        button2: { text: "Tournament details" },
-        icon: "/images/tournament-web-images/Union.svg"
-      },
-      // Additional slides here...
-    ];
+    const banner = {
+    title: data.banner.title ? data.banner.title : "BMPS 2024",
+    description: data.banner.description,
+    logo: data.banner.logo,
+    backgroundUrl: data.banner.background,
+    isLargeBackground: false,
+    largeBackgroundUrl: data.banner.largeBackgroundUrl, 
+    button1: { text: "Watch Live", url: data.banner.liveChannerUrl},
+    button2: { text: "Tournament details", url:"#info" }
+  }
+      
   </script>
-  
-  <CardSlider options={splideOptions}>
-    {#each banners as banner}
-      <SplideSlide>
+  {#if banner.isLargeBackground && banner.largeBackgroundUrl}
+    <div class="main-bg bg-cover" style="background-image: url({banner.largeBackgroundUrl ? banner.largeBackgroundUrl : "https://api.dev.kie.tesseractesports.com/assets/cms_assets/main_banner_bg.png"});">
+      <Section>
         <div class="flex items-start w-full">
           <div class="h-full w-full">
             <div class="bg-image flex max-h-full max-w-full flex-col bg-no-repeat">
               <div class="z-0 flex flex-col justify-end gap-y-4 bg-cover bg-center pb-16 lg:pb-28 pt-[320px] lg:pt-[420px] w-f">
-                <img src={banner.image} alt="" class="z-10 h-[120px] w-24 flex-shrink-0">
+                <img src={banner.logo} alt="" class="z-10 h-[120px] w-24 flex-shrink-0">
                 <div class="flex items-end pt-2">
                   <div class="font-manrope flex-grow text-[56px] font-extrabold leading-[normal] text-yellow-400">
                     {banner.title}
@@ -55,34 +37,39 @@
                 <div class="flex flex-wrap items-end justify-between gap-x-6 gap-y-5 ">
                   <div class="flex flex-wrap items-center justify-center gap-x-6 gap-y-5 leading-6 ">
                     <a href="" class="flex items-center justify-center gap-x-3 rounded-md border border-solid border-neutral-600 bg-neutral-800 px-3 py-2 drop-shadow-lg">
-                      <img alt="" src={banner.button1.icon} class="h-4 w-4 flex-shrink-0" />
+                      <span class="material-symbols-outlined text-red-700">
+                        videocam
+                        </span>
                       <div class="text-[silver]">{banner.button1.text}</div>
                     </a>
                     <a href="" class="rounded-md bg-yellow-400 px-3 py-2 text-center text-neutral-900 drop-shadow-lg">
                       {banner.button2.text}
                     </a>
                   </div>
-                  <button class="flex h-12 w-12 flex-shrink-0 flex-col items-center absolute top-6 right-6 md:relative justify-center rounded-full bg-neutral-800 p-3 ml-3"> 
-                    <img alt="" src={banner.icon} class="h-6 w-6 flex-shrink-0" />
-                </button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </SplideSlide>
-    {/each}
-  </CardSlider>
+      </Section>
+    </div>
+    {:else}
+    <div class="main-bg bg-cover" style="background-image: url({banner.backgroundUrl ? banner.backgroundUrl : "https://dev-kie-tmp-docs.s3.ap-south-1.amazonaws.com/tournament-page-assets/BGMI_background.png"});">
+      <Section>
+        <div class="h-80 w-full py-16 justify-start items-center gap-2.5 inline-flex">
+          <div class="grow shrink basis-0 h-48 justify-start items-center gap-2.5 flex">
+            <div class="grow shrink basis-0 flex-col justify-start items-start gap-2.5 inline-flex">
+              <div class="self-stretch text-white text-6xl font-extrabold leading-10">BMPS 2024</div>
+              <div class="self-stretch text-white text-xl font-semibold leading-loose">BATTLEGROUNDS MOBILE PRO SERIES 2024</div>
+            </div>
+            <div class="w-44 h-48 relative items-center justify-end hidden sm:flex">
+              <img src={banner.logo} alt="" class="">
+            </div>
+          </div>
+        </div>
+      </Section>
+    </div>
+  {/if}
   
-
-  <style>
-    /* Custom pagination dot colors */
-    :global(.splide__pagination__page) {
-      background-color: #B9B8B8 !important; /* Change to your desired color */
-    }
-  
-    /* Active dot color */
-    :global(.splide__pagination__page.is-active) {
-      background-color: #FFD700 !important; /* Change to active color */
-    }
-  </style>
+    
+    
