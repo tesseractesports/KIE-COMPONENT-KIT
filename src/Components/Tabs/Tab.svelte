5a818378baa4768c5188a029c7b229485f4ad515
azrajesh0007 <rajesh.azmeera@tesseractesports.com>
2024-11-18 23:52:27 +0530

    updated

--- src/Components/Tabs/Tab.svelte
+++ src/Components/Tabs/Tab.svelte
@@ -12,17 +12,17 @@
 </script>
 
 <button
-  class="border-transparent text-gray-500 hover:border-gray-200 hover:text-indigo-600 flex whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium {$selectedTab === tab ? "text-indigo-600 border-b-2": ""}"
+  class="border-transparent  uppercase hover:border-gray-200 flex whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium {$selectedTab === tab ? "text-yellow-400 border-b-2 border-yellow-400": "text-Charcoal/100"}"
   class:selected={$selectedTab === tab}
   on:click={() => selectTab(tab)}
 >
   <slot />
-  {#if Count}
+  <!-- {#if Count}
     <span
       class="bg-gray-100 text-gray-900 ml-3 hidden rounded-full py-0.5 px-2.5 text-xs font-medium md:inline-block {$selectedTab === tab ? "text-indigo-600 bg-indigo-100": ""}"
       >{Count}</span
     >
-  {/if}
+  {/if} -->
 </button>
 
 <style>
