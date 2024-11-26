de63aca2eb6135af1f32d0bf09e9bff3974fa3fa
azrajesh0007 <rajesh.azmeera@tesseractesports.com>
2024-11-26 19:05:54 +0530

    updated

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,17 @@
 <script>
 	import PrizepoolKIT from './components/PrizepoolKIT.svelte';
 	import InfoSectionKIT from './components/InfoSectionKIT.svelte';
-  import BannerSectionKit from './components/BannerSectionKIT.svelte';
+  import BannerSectionKIT from './components/BannerSectionKIT.svelte';
   import LeaderBoardKit from './Components/LeaderBoardKIT.svelte';
   import OverviewKit from './components/OverviewKIT.svelte';
   import SectionKit from './components/SectionKIT.svelte';
+  import RegisterNowKIT from './Components/RegisterNowKIT.svelte';
 </script>
 <div class=" bg-Charcoal/900">
-  <BannerSectionKit />  
+  <BannerSectionKIT />  
   <InfoSectionKIT />
   <PrizepoolKIT />
+  <RegisterNowKIT />
 </div>
 
 
