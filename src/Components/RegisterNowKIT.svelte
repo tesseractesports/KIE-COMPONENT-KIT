8ed106671d2663cba3958d873639eacaed287b74
azrajesh0007 <rajesh.azmeera@tesseractesports.com>
2024-11-27 15:19:38 +0530

    updatedf

--- src/Components/RegisterNowKIT.svelte
+++ src/Components/RegisterNowKIT.svelte
@@ -7,12 +7,14 @@
   $:registerdata={
     title:$webConfig?.registerNowSection?.title,
     description:$webConfig?.registerNowSection?.description,
-    logo:$webConfig?.registerNowSection?.logo,
+    background:$webConfig?.registerNowSection?.background,
+    isPrivate:$webConfig?.registerNowSection?.isPrivate,
+    privateURL:$webConfig?.registerNowSection?.privatePageUrl
   }
 
   </script>
   
-  <div class="bg-no-repeat bg-cover bg-center" style="background-image: url({registerdata.logo ||'/api/placeholder/1920x1080'});">
+  <div class="bg-no-repeat bg-cover bg-center" style="background-image: url({registerdata.background ||'/api/placeholder/1920x1080'});">
       <SectionKit>
           <div class="relative z-0 flex flex-col items-start justify-center gap-y-3 py-24 font-medium" >
               <h1 class=" self-stretch text-4xl font-extrabold leading-10 text-yellow-400"
@@ -27,11 +29,20 @@
                 </p>
               </div>
               <div class="flex items-end pt-4">
-                <a href="" class="rounded-md bg-yellow-400 px-8 py-2 text-center leading-6 text-neutral-900 drop-shadow-lg"
-                 style="background-color: {theme.colors.primary}; color: {theme.colors.background};"
-                >
-                  Register Now
-                </a>
+                {#if registerdata.isPrivate}
+                  <a href="{registerdata.privateURL}" target="_blank" class="rounded-md bg-yellow-400 px-8 py-2 text-center leading-6 text-neutral-900 drop-shadow-lg"
+                  style="background-color: {theme.colors.primary}; color: {theme.colors.background};"
+                  >
+                    Register Now
+                  </a>
+                  {:else} 
+                  <button on:click class="rounded-md bg-yellow-400 px-8 py-2 text-center leading-6 text-neutral-900 drop-shadow-lg"
+                  style="background-color: {theme.colors.primary}; color: {theme.colors.background};"
+                  >
+                    Register Now
+                </button>
+                {/if}
+                
               </div>
             </div>
       </SectionKit>
